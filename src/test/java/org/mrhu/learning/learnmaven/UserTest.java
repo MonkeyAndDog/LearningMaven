c451d6033fb707f1cb9ace2824fd0c81b55da819
Monkey <1466947023@qq.com>
2018-04-17 22:59:04 +0800

    提交测试修改

---
+++ src/test/java/org/mrhu/learning/learnmaven/UserTest.java
@@ -0,0 +1,17 @@
+package org.mrhu.learning.learnmaven;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UserTest {
+
+    @Test
+    public void getName() {
+        System.out.println("666");
+    }
+
+    @Test
+    public void setName() {
+    }
+}
